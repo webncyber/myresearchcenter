8844f0ba5217dc0dad7ffd38db0ed35823a3f5d0
nazo <webncyber@yahoo.com>
2022-12-17 15:22:36 -0500

    add home js

---
+++ src/pages/Home.js
@@ -0,0 +1,28 @@
+import logo from '../logo.svg';
+import '../App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          This is Home page! Learn React.....
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
