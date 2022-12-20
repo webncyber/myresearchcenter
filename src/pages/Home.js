c6e1addb66a3bc31c32818fb8d335cd97b86f89e
nazo <webncyber@yahoo.com>
2022-12-19 23:06:31 -0500

    add flex style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,15 @@
-import logo from '../logo.svg';
-import '../App.css';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import '../Styles/Layout.scss'
+import {Container, Row, Col} from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          This is Home page! Learn React.....
-        </a>
-      </header>
-    </div>
+   <div className='container'>
+      <div className='row'>
+        <div className='col'>Col1 </div>
+        <div className='col'>Col 2 </div>
+      </div>
+   </div>
   );
 }
 
