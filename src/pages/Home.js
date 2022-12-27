947cefc3440af92f07acc8a5c1b10196ac285f62
SavvasNazarethTajrian <nazo.tajrian@savvas.com>
2022-12-27 16:38:12 -0500

    add content section component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,43 +3,32 @@ import Hero from '../components/Hero/'
 import NavBar from '../components/navbar';
 import homeImagePath from '../Images/bkgHome.jpg'
 import { Helmet } from 'react-helmet';
+import SingleColumnContent from '../components/singleColumnContent'
 
 
 function App() {
   return (
     
-   <div >
+   <div className='container'>
     <Helmet>
         <meta charSet='utf-8'/>
         <title>My Research Center</title>
         <link rel="canonical" href="" />
       </Helmet>
-      <Hero url={homeImagePath} headingOne="Ideas.........." headingTow="are where your research begins!" />
-      <NavBar/>
-      <div>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
+
+      <div className='heading-section'>
+        <NavBar/>
+        <Hero url={homeImagePath} headingOne="Ideas.........." headingTow="are where your research begins!" />
       </div>
+      <div className='content-section'>
+          <SingleColumnContent/>
+      </div>
+
+      {/* <Hero url={homeImagePath} headingOne="Ideas.........." headingTow="are where your research begins!" />
+      <NavBar/>
+
+      <SingleColumnContent/> */}
+    
    </div>
   );
 }
